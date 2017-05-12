3c90b049a350e0be474357eb28c0f61d90bc7cfd
Pushkar Pradhan <ppradhan@yahoo-inc.com>
2017-05-12 14:44:51 -0700

    Adding one not ok

--- runtests.sh
+++ runtests.sh
@@ -2,5 +2,5 @@
 
 echo "test_choice (__main__.TestSequenceFunctions) ... ok"
 echo "test_sample (__main__.TestSequenceFunctions) ... ok"
-echo "test_shuffle (__main__.TestSequenceFunctions) ... ok"
+echo "test_shuffle (__main__.TestSequenceFunctions) ... not ok"
 
