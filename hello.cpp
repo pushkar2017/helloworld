f5392560a91de7cf7df856e474bf30e333dd7d6b
Pushkar Pradhan <ppradhan@yahoo-inc.com>
2017-05-12 12:15:59 -0700

    Return failure if more than 1 arg is passed

--- hello.cpp
+++ hello.cpp
@@ -3,11 +3,15 @@ using namespace std;
 
 int main(int argc, char ** argv) {
   
-  if (argc > 1) {
+  if (argc == 2) {
     cout << "hello " << argv[1] << endl;
   }
-  else {
+  else if (argc == 1) {
     cout << "hello world" << endl;
+  }
+  else {
+    cout << "Usage: " << argv[0] << " [name]" << endl;
+    return 1;
   }
   return 0;
 }
