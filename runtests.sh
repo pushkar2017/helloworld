fef54d8a48057a4a8647a88cc72a2f74a401d71a
Pushkar Pradhan <ppradhan@yahoo-inc.com>
2017-05-12 14:51:02 -0700

    Using TAP format

--- runtests.sh
+++ runtests.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-echo "test_choice (__main__.TestSequenceFunctions) ... ok"
-echo "test_sample (__main__.TestSequenceFunctions) ... ok"
-echo "test_shuffle (__main__.TestSequenceFunctions) ... not ok"
-
+echo "1..2"
+echo "ok 1"
+echo "not ok 2"
